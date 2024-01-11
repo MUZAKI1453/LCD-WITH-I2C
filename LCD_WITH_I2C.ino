87b1872e31d38e61c529fe91bd211f7d9c16535b
MUZAKI1453 <124240121+MUZAKI1453@users.noreply.github.com>
2024-01-11 19:08:13 +0700

    Add files via upload

---
+++ LCD_WITH_I2C.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
+
+hd44780_I2Cexp lcd;
+
+void setup() {
+  lcd.begin(16, 2); // Inisialisasi LCD dengan 16 kolom dan 2 baris
+  lcd.print(F("Subscribe my YT"));
+  lcd.setCursor(0, 1);
+  lcd.print(F("Mr Arduino Noobs"));
+}
+
+void loop() {
+  // Kode loop utama (jika diperlukan)
+}
