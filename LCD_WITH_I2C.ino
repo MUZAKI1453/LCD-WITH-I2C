617ed7c78395ab906ea610e56177d6272e3b5c31
Muhamad Arif Muzaki <124240121+MUZAKI1453@users.noreply.github.com>
2024-10-22 05:22:32 +0300

    Update LCD_WITH_I2C.ino

--- LCD_WITH_I2C.ino
+++ LCD_WITH_I2C.ino
@@ -8,7 +8,7 @@ void setup() {
   lcd.begin(16, 2); // Inisialisasi LCD dengan 16 kolom dan 2 baris
   lcd.print(F("Subscribe my YT"));
   lcd.setCursor(0, 1);
-  lcd.print(F("Mr Arduino Noobs"));
+  lcd.print(F("Mr Controllers Noobs"));
 }
 
 void loop() {
